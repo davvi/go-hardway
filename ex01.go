bc8d86c9d673a049beb8b027d993c3f5dc3178ee
davvi <linx82@gmail.com>
2016-05-22 18:44:52 +1000

    First one done

---
+++ ex01.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello World")
+    fmt.Println("Hello Again")
+    fmt.Println("I like typing this.")
+    fmt.Println("This is fun.")
+    fmt.Println("Yay! Printing.")
+    fmt.Println("I'd must rather you 'not'.")
+    fmt.Println("I \"said\" do not touch this.")
+}
